f0de9b9276d884ae4b40c5a1faf3cd42952f3567
Brett Falk <fbrett@cis.upenn.edu>
2025-10-02 13:20:17 -0400

    to_wei

--- docker/app.py
+++ docker/app.py
@@ -104,7 +104,7 @@ for i, (name, account) in enumerate(accounts.items()):
             "from": account.address,
             "value": 0,
             "gas": 500000,
-            "gasPrice": w3.toWei("1", "gwei"),
+            "gasPrice": w3.to_wei("1", "gwei"),
             "nonce": w3.eth.getTransactionCount(account.address),
         })
         signed_tx = account.sign_transaction(tx)
